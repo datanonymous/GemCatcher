3d706975f26e7400b3f3db69546104c6828f3fc6
Alex Ko <ko.e.alexander@gmail.com>
2024-12-26 21:48:18 -0500

    As of Dec 26, 2024

---
+++ scenes/paddle.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@export var speed: float = 500.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#if Input.is_action_pressed("left"):
+		#position.x += -speed * delta
+	#if Input.is_action_pressed("right"):
+		#position.x += speed * delta
+	var input_amount: float = Input.get_axis("left", "right")
+	position.x += delta * speed * input_amount
+	
+	
+	
